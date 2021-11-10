ea8bbad7420228ddd248c081361ec1620c6da516
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-10 12:17:42 -0600

    Agregando el tipado de getgiphyresponse a get

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { GetGiphyResponse } from '../interfaces/gifs.interface';
+import { GetGiphyResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +10,7 @@ export class GifsService {
   private apiKey: string = 'u9XMpJbDbobrYaQwDbQYTkFJ1xCWHnhg';
   private _historial: string[] = [];
 
-  public results: any[] = [];
+  public results: Gif[] = [];
 
   get historial(): string[] {
     return [...this._historial];
@@ -23,8 +23,9 @@ export class GifsService {
     if (this._historial.includes(query)) return;
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
-      .subscribe((resp: any) => {
+    this.http.get<GetGiphyResponse>
+      (`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((resp) => {
         console.log(resp.data)
         this.results = resp.data;
       });
