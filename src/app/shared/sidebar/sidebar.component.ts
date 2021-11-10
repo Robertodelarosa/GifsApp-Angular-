7b491e5349a769e0833f985745d2901f48938793
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-10 10:59:51 -0600

    Otra implementacion para cortar el historial localmente

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -11,6 +11,8 @@ export class SidebarComponent {
 
   get historial() {
     return this.gifsService.historial
+    //Si se implementa aqui solo la información mostrada es manipulada y no se pierde información.
+    // return this.gifsService.historial.slice(0, 10);
   }
 
   constructor(private gifsService: GifsService) { }
