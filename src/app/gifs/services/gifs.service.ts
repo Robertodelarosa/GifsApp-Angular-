3165773e0e1b67c4279b7ac9a52ca4ceadc85329
Roberto De la Rosa Acosta <robertodelarosa117@gmail.com>
2021-11-10 12:33:57 -0600

    Usando localStorage para que persista el historial

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -16,13 +16,22 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [] //Forma uno
+    // if (localStorage.getItem('historial')) {  //Forma dos
+    //   this._historial = JSON.parse(localStorage.getItem('historial')!);
+    // }
+  }
 
   buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
     if (this._historial.includes(query)) return;
+
     this._historial.unshift(query);
     this._historial = this._historial.splice(0, 10);
+
+    localStorage.setItem('historial', JSON.stringify(this._historial));
+
     this.http.get<GetGiphyResponse>
       (`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
       .subscribe((resp) => {
